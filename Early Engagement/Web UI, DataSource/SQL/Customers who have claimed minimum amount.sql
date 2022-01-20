c1af128414f421efa9443a47a16919e7a3b5a982
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2022-01-20 20:13:02 +0530

    Create Customers who have claimed minimum amount.sql

---
+++ Early Engagement/Web UI, DataSource/SQL/Customers who have claimed minimum amount.sql
@@ -0,0 +1,6 @@
+select top 1 cust.first_name 
+from customer cust
+    inner join customer_policy pol on cust.id=pol.customer_id
+    inner join claims cl on pol.id=cl.customer_policy_id
+order by cl.amount_of_claim asc
+GO
